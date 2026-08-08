cc3270ae4b25f9836741ec9c545da5271c65c47f
Danil <noskovde2015@gmail.com>
2026-08-08 10:32:45 +0300

    add settings and main file

---
+++ app/core/config.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    host: str = "127.0.0.3"
+    port: int = 8003
+
+
+class ApiPrefix(BaseModel):
+    pass
+
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent.parent / ".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        env_prefix="APP_CONFIG__",
+        env_nested_delimiter="__"
+    )
+    run: RunConfig = RunConfig()
+    api_prefix: ApiPrefix = ApiPrefix()
+    db: DataBaseConfig
+
+
+settings = Settings()
+
+
+
---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import uvicorn
+from core.config import settings
+
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app",host=settings.run.host, port=settings.run.port, reload=True)
