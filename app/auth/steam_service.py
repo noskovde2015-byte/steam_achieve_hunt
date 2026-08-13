bb001929de3bd43549d18f00f8ff39975d2fb4b5
Danil <noskovde2015@gmail.com>
2026-08-13 11:09:09 +0300

    update steam_service.py: add fun for install avatar and username

--- app/auth/steam_service.py
+++ app/auth/steam_service.py
@@ -5,6 +5,8 @@ from core.models import User
 from urllib.parse import urlencode
 from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
+from app.core.config import settings
+import json
 
 
 def build_steam_login_url(return_to: str, realm: str) -> str:
@@ -26,7 +28,9 @@ async def verify_steam_response(params: dict) -> str | None:
     params["openid.mode"] = "check_authentication"
 
     async with httpx.AsyncClient() as client:
-        response = await client.post("https://steamcommunity.com/openid/login", data=params)
+        response = await client.post(
+            "https://steamcommunity.com/openid/login", data=params
+        )
 
     if "is_valid:true" in response.text:
         parsed = params["openid.claimed_id"]
@@ -36,19 +40,31 @@ async def verify_steam_response(params: dict) -> str | None:
         return None
 
 
+async def get_player_summary(steam_id: str) -> dict:
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/",
+            params={"steamids": steam_id, "key": settings.steam.api_key},
+        )
+    data = response.json()
+    return data["response"]["players"][0]
+
 
 async def get_or_create_user(steam_id: str, session: AsyncSession) -> User:
     stmt = select(User).where(User.steam_id == steam_id)
     result = await session.execute(stmt)
     user = result.scalar_one_or_none()
 
+    user_info = await get_player_summary(steam_id=steam_id)
+
     if user is None:
-        new_user = User(steam_id=steam_id)
-        session.add(new_user)
-        await session.commit()
-        await session.refresh(new_user)
-        return new_user
-    else:
-        return user
+        user = User(steam_id=steam_id)
+        session.add(user)
 
+    user.username = user_info["personaname"]
+    user.avatar_url = user_info["avatarfull"]
 
+    await session.commit()
+    await session.refresh(user)
+    return user
