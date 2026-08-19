ecbe8a47444f22c5317099a1806a9033c80381ce
Danil <noskovde2015@gmail.com>
2026-08-19 10:06:02 +0300

    Include Profile router

--- app/api_router.py
+++ app/api_router.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter
 from app.core.config import settings
 from app.auth.router import router as auth_router
 from app.game_info.router import router as game_info_router
+from app.profile.router import router as profile_router
 
 api_router = APIRouter(prefix=settings.api_prefix.prefix)
 api_router.include_router(auth_router)
 api_router.include_router(game_info_router)
+api_router.include_router(profile_router)
